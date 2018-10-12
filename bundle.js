149ecd5fcac44073427b73d9d63d65e4a39297d9
Jamie Kinkead <jkinkead@gmail.com>
2018-10-12 09:09:05 -0700

    minor tweaks

--- bundle.js
+++ bundle.js
@@ -17153,7 +17153,7 @@ const _ = require('lodash')
 
 
 const renderScore = () => {
-    let displayScore = localStorage.getItem('score')
+    let displayScore = localStorage.getItem('score') || 0
     const scorebox = document.querySelector('.scorebox')
     scorebox.innerHTML = `<p class="score">Wins: ${displayScore}</p>`
 }
